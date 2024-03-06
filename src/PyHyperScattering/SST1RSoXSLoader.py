686270118d2ba308e6c2fed0ecfda9d055b752d5
Phong <phonghnguyen97@gmail.com>
2024-03-05 18:10:04 -0800

    Minor changes to img dimensions

--- src/PyHyperScattering/SST1RSoXSLoader.py
+++ src/PyHyperScattering/SST1RSoXSLoader.py
@@ -80,11 +80,10 @@ class SST1RSoXSLoader(FileLoader):
         if use_cached_md != False:
             raise NotImplementedError('Caching of metadata is not supported for SST1')
    
-        # Use chunked loading if flag is set
         if self.use_chunked_loading:
             img = imread(str(filepath))
             if img.ndim == 3:
-                img = img[:, :, 0]  # Select the first channel if the image is multi-channel
+                img = img[0, :, :]
         else:
             img = np.array(Image.open(filepath))
 
@@ -132,19 +131,10 @@ class SST1RSoXSLoader(FileLoader):
 
     def read_json(self,jsonfile):
         json_dict = {}
-
-        # Try / except statment to be compatible with local rsoxs data before 
-        # and after SST1 cycle 2 2023 
-        try:  # For earlier cyles
-            with open(jsonfile) as f:
-                data = json.load(f)
-                meas_time = datetime.datetime.fromtimestamp(data[1]['time'])
-        except KeyError:  # For later cycles (confirmed working for cycle 2 2023) 
-            with open(jsonfile) as f:
-                data = [0, json.load(f)]  # Quick fix to load the json in a list 
-                meas_time = datetime.datetime.fromtimestamp(data[1]['time'])
-            
-        json_dict['sample_name'] = data[1]['sample_name']
+        with open(jsonfile) as f:
+            data = json.load(f)
+            meas_time =datetime.datetime.fromtimestamp(data[1]['time'])
+            json_dict['sample_name'] = data[1]['sample_name']
         if data[1]['RSoXS_Main_DET'] == 'SAXS':
             json_dict['rsoxs_config'] = 'saxs'
             # discrepency between what is in .json and actual
@@ -166,7 +156,7 @@ class SST1RSoXSLoader(FileLoader):
                 json_dict['beamcenter_y'] = data[1]['RSoXS_SAXS_BCY']
                 json_dict['sdd'] = data[1]['RSoXS_SAXS_SDD']
 
-        elif (data[1]['RSoXS_Main_DET'] == 'WAXS') | (data[1]['RSoXS_Main_DET'] == 'waxs_det'):  # additional name for for cycle 2 2023
+        elif data[1]['RSoXS_Main_DET'] == 'WAXS':
             json_dict['rsoxs_config'] = 'waxs'
             if (meas_time > datetime.datetime(2020,11,16)) and (meas_time < datetime.datetime(2021,1,15)):
                 json_dict['beamcenter_x'] = 400.46
@@ -183,7 +173,7 @@ class SST1RSoXSLoader(FileLoader):
                 json_dict['sdd'] = data[1]['RSoXS_WAXS_SDD']
 
         else:
-            json_dict['rsoxs_config'] = 'unknown'
+            json_dict['rsoxs_config'] == 'unknown'
             warnings.warn('RSoXS_Config is neither SAXS or WAXS. Check json file',stacklevel=2)
 
         if json_dict['sdd'] == None:
@@ -263,15 +253,9 @@ class SST1RSoXSLoader(FileLoader):
         else:
             cwd = pathlib.Path(dirPath)
 
-        # Another try/except statement to be compatible with local data pre and 
-        # post SST1 cycle 2 2023 
-        try:  # For earlier data
-            json_fname = list(cwd.glob('*.jsonl'))
-            json_dict = self.read_json(json_fname[0])
-        except IndexError:  # For later data (works for cycle 2 2023)
-            json_fname = list(cwd.glob('*.json')) # Changed '*.jsonl' to '*.json' 
-            json_dict = self.read_json(json_fname[0]) 
-        
+        json_fname = list(cwd.glob('*.jsonl'))
+        json_dict = self.read_json(json_fname[0])
+
         baseline_fname = list(cwd.glob('*baseline.csv'))
         baseline_dict = self.read_baseline(baseline_fname[0])
 
